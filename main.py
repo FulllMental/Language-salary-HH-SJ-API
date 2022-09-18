578e32ac0d7e8934ac08a35fac9f681ecdb7d0c2
FulllMental <fulllmental@gmail.com>
2022-09-18 20:59:49 +0300

    Add predict rub salary script

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests
 
 
-def get_hh_vacancies(programming_language):
+def get_api_response(programming_language):
     payload = {
         'text': f'программист {programming_language}',
         'area': '1',
@@ -17,10 +17,25 @@ def get_hh_vacancies(programming_language):
 def count_vacancies(programming_languages):
     language_vacancies = {}
     for language in programming_languages:
-        found = get_hh_vacancies(language).json()["found"]
+        found = get_api_response(language).json()["found"]
         if found > 99:
             language_vacancies.update({language: found})
     return language_vacancies
+
+
+def predict_rub_salary(vacancy):
+    salary_from = vacancy['salary']['from']
+    salary_to = vacancy['salary']['to']
+
+    if vacancy['salary']['currency'] == 'RUR':
+        if salary_to and salary_from:
+            return salary_from + salary_to / 2
+        elif salary_to:
+            return salary_to * 0.8
+        elif salary_from:
+            return salary_from * 1.2
+    else:
+        return None
 
 
 if __name__ == '__main__':
@@ -39,5 +54,5 @@ if __name__ == '__main__':
         'TypeScript'
     ]
 
-    for vacancy in get_hh_vacancies('Python').json()['items']:
-        print(vacancy['salary'])
+    for vacancy in get_api_response('Python').json()['items']:
+        print(predict_rub_salary(vacancy))
