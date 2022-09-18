0779f499a8d7b28651cbab4262e1259fdc3ff19b
FulllMental <fulllmental@gmail.com>
2022-09-18 17:17:12 +0300

    Add vacancy language filter

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import requests
 
 
-def get_hh_vacancies():
+def get_hh_vacancies(programming_language):
     payload = {
-        'text': f'программист',
+        'text': f'программист {programming_language}',
         'area': '1',
         'period': '30',
     }
@@ -13,5 +13,29 @@ def get_hh_vacancies():
     return response
 
 
+def count_vacancies(programming_languages):
+    language_vacancies = {}
+    for language in programming_languages:
+        found = get_hh_vacancies(language).json()["found"]
+        if found > 99:
+            language_vacancies.update({language: found})
+    return language_vacancies
+
+
 if __name__ == '__main__':
-    print(get_hh_vacancies().json())
+    programming_languages = [
+        'JavaScript',
+        'Java',
+        'Python',
+        'Ruby',
+        'C++',
+        'C#',
+        'C',
+        'Go',
+        'Objective-C',
+        'Scala',
+        'Swift',
+        'TypeScript'
+    ]
+    print(count_vacancies(programming_languages))
+
