a95784af30b28e75da0742d16d682b262737de0d
FulllMental <104234625+FulllMental@users.noreply.github.com>
2022-10-10 16:54:11 +0300

    Change first request

--- main.py
+++ main.py
@@ -1,22 +1,6 @@
 import requests
 
 
-def get_all_vacancies(programming_language):
-    page = 0
-    pages_number = 1
-    total_vacancies = []
-    while page < pages_number:
-        page_response = get_api_response(programming_language, page)
-        total_found = page_response['found']
-        if total_found > 99:
-            pages_number = page_response['pages']
-            for vacancies in page_response['items']:
-                total_vacancies.append(vacancies)
-            page += 1
-        else: break
-    return get_average_info_hh(programming_language, total_vacancies, total_found)
-
-
 def get_api_response(language, page):
     payload = {
         'text': f'Программист {language}',
@@ -32,25 +16,33 @@ def get_api_response(language, page):
     return response.json()
 
 
+def get_all_vacancies(programming_language, page, pages_number, total_vacancies):
+    while page < pages_number:
+        page_response = get_api_response(programming_language, page)
+        page_items = page_response['items']
+        for vacancies in page_items:
+            total_vacancies.append(vacancies)
+        page += 1
+    return total_vacancies
+
+
 def get_average_info_hh(programming_language, total_vacancies, total_found):
-    language_vacancies = {}
-    if total_found > 99:
-        average_salary = [predict_rub_salary(vacancy) for vacancy in total_vacancies if vacancy['salary']['currency'] == 'RUR']
-        vacancies_processed = len(average_salary)
-        language_vacancies.update(
-            {
-                programming_language: {
-                    'vacancies_found': total_found,
-                    'vacancies_processed': vacancies_processed,
-                    'average_salary': int(sum(average_salary)/vacancies_processed)
-                }
+    average_salary = [predict_rub_salary(vacancy) for vacancy in total_vacancies if
+                      vacancy['salary']['currency'] == 'RUR']
+    vacancies_processed = len(average_salary)
+    language_vacancies.update(
+        {
+            programming_language: {
+                'vacancies_found': total_found,
+                'vacancies_processed': vacancies_processed,
+                'average_salary': int(sum(average_salary) / vacancies_processed)
             }
-        )
+        }
+    )
     return language_vacancies
 
 
 def predict_rub_salary(vacancy):
-
     salary_from = vacancy['salary']['from']
     salary_to = vacancy['salary']['to']
 
@@ -60,7 +52,8 @@ def predict_rub_salary(vacancy):
         return salary_to * 0.8
     elif salary_from:
         return salary_from * 1.2
-    else: None
+    else:
+        None
 
 
 if __name__ == '__main__':
@@ -79,7 +72,18 @@ if __name__ == '__main__':
         'TypeScript'
     ]
 
-    hh_table = {}
-    for programming_language in programming_languages:
-        hh_table.update(get_all_vacancies(programming_language))
-    print(hh_table)
+    language_vacancies = {}
+    for language in programming_languages:
+        page = 0
+        first_page = get_api_response(language, page)
+        total_found = first_page['found']
+
+        if total_found > 99:
+            pages_number = first_page['pages']
+            total_vacancies = [vacancies for vacancies in first_page['items']]
+            get_all_vacancies(language, page + 1, pages_number, total_vacancies)
+            get_average_info_hh(language, total_vacancies, total_found)
+        else:
+            continue
+
+    print(language_vacancies)
